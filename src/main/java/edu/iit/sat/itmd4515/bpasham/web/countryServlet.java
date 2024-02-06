1970443e96b2cfaad401f4e02d7463071c8ccd33
Bhanumahesh <bhanumahesh70@gmail.com>
2024-02-05 20:30:43 -0600

    lab-3 jsp created and HTML form added

--- src/main/java/edu/iit/sat/itmd4515/bpasham/web/countryServlet.java
+++ src/main/java/edu/iit/sat/itmd4515/bpasham/web/countryServlet.java
@@ -4,14 +4,31 @@
  */
 package edu.iit.sat.itmd4515.bpasham.web;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.Logger;
 
 /**
  *
  * @author bhanu
  */
-@WebServlet(name = "DemoServlet", urlPatterns = {"/country","/Country","/c"})
-public class countryServlet extends HttpServlet{
+@WebServlet(name = "CountryServlet", urlPatterns = {"/country","/Country","/c"})
+public class CountryServlet extends HttpServlet{
+
+    private static final Logger LOG = Logger.getLogger(CountryServlet.class.getName());
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+         LOG.info("CountryServlet in doPost");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    LOG.info("CountryServlet in doGet");
+    }
     
 }
