a4235fc27ee28a889b73a956849ec593f7ec5f85
Bhanumahesh <bhanumahesh70@gmail.com>
2024-02-19 23:03:28 -0600

    lab-5 order to customer relationship added

--- src/main/java/edu/iit/sat/itmd4515/bpasham/domain/Customer.java
+++ src/main/java/edu/iit/sat/itmd4515/bpasham/domain/Customer.java
@@ -4,19 +4,24 @@
  */
 package edu.iit.sat.itmd4515.bpasham.domain;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.criteria.Order;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -29,6 +34,8 @@ import java.util.Objects;
 public class Customer {
       @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+      
+      @Column(name = "customer_id")
     private Long customerId;
 
     @NotBlank
@@ -40,6 +47,9 @@ public class Customer {
     @NotNull
     private LocalDate createdAt;
 
+    
+   @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Order> orders = new ArrayList<>();
    
     public Customer() {
     }
--- src/main/java/edu/iit/sat/itmd4515/bpasham/domain/Order.java
+++ src/main/java/edu/iit/sat/itmd4515/bpasham/domain/Order.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -33,6 +34,10 @@ public class Order {
 
     @Min(1)
     private Integer quantity;
+    
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
     
     public Order() {
     }
