4350ff5720c87a62c54a569820edd1dfea337a1f
Bhanumahesh <bhanumahesh70@gmail.com>
2024-03-18 16:38:47 -0500

    lab-6 working

---
+++ src/main/java/edu/iit/sat/itmd4515/bpasham/service/OrderService.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.iit.sat.itmd4515.bpasham.service;
+
+import edu.iit.sat.itmd4515.bpasham.domain.Beverage;
+import edu.iit.sat.itmd4515.bpasham.domain.Customer;
+import jakarta.ejb.Stateless;
+import java.util.List;
+
+/**
+ *
+ * @author bhanu
+ */
+@Stateless
+public class CustomerService extends AbstractService<Customer>{
+    public CustomerService() {
+        super(Customer.class);
+    }
+    
+    
+    public List<Customer> findAll(){
+        return super.findAll("Customer.findAll");
+    }
+}
