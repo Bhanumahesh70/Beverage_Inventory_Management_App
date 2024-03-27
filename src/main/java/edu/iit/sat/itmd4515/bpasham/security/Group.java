fb6e8e3aaf974db5e2668d6e9aef6d2b32cd64e4
Bhanumahesh <bhanumahesh70@gmail.com>
2024-03-27 13:04:12 -0500

    lab-8 security layer added

---
+++ src/main/java/edu/iit/sat/itmd4515/bpasham/security/Group.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.iit.sat.itmd4515.bpasham.security;
+
+import edu.iit.sat.itmd4515.bpasham.security.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author bhanu
+ */
+@Entity
+@Table(name = "SEC_TABLE")
+public class Group {
+
+    @Id
+    private String groupName;
+
+    private String groupDescription;
+    
+    @ManyToMany(mappedBy = "groups")
+    private List<User> users= new ArrayList<>();
+
+    /**
+     * Get the value of users
+     *
+     * @return the value of users
+     */
+    public List<User> getUsers() {
+        return users;
+    }
+
+    /**
+     * Set the value of users
+     *
+     * @param users new value of users
+     */
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+
+    /**
+     * Get the value of groupDescription
+     *
+     * @return the value of groupDescription
+     */
+    public String getGroupDescription() {
+        return groupDescription;
+    }
+
+    /**
+     * Set the value of groupDescription
+     *
+     * @param groupDescription new value of groupDescription
+     */
+    public void setGroupDescription(String groupDescription) {
+        this.groupDescription = groupDescription;
+    }
+
+    /**
+     * Get the value of groupName
+     *
+     * @return the value of groupName
+     */
+    public String getGroupName() {
+        return groupName;
+    }
+
+    /**
+     * Set the value of groupName
+     *
+     * @param groupName new value of groupName
+     */
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+}
---
+++ src/main/java/edu/iit/sat/itmd4515/bpasham/security/User.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.iit.sat.itmd4515.bpasham.security;
+
+import edu.iit.sat.itmd4515.bpasham.security.Group;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author bhanu
+ */
+@Entity
+@Table(name = "SEC_USER")
+public class User {
+
+    @Id
+    private String userName;
+
+    private String password;
+    
+    @ManyToMany
+    @JoinTable(name="SEC_USER_GROUPS",
+            joinColumns = @JoinColumn(name="USERNAME"),
+            inverseJoinColumns = @JoinColumn(name="GROPUNAME"))
+       private List<Group> groups = new ArrayList<>();
+    
+    public void addGroup(Group g ){
+        this.groups.add(g);
+        g.getUsers().add(this);
+    }
+
+    public void removeGroup(Group g ){
+        this.groups.remove(g);
+        g.getUsers().remove(this);
+    }
+    /**
+     * Get the value of groups
+     *
+     * @return the value of groups
+     */
+    public List<Group> getGroups() {
+        return groups;
+    }
+
+    /**
+     * Set the value of groups
+     *
+     * @param groups new value of groups
+     */
+    public void setGroups(List<Group> groups) {
+        this.groups = groups;
+    }
+
+
+    /**
+     * Get the value of password
+     *
+     * @return the value of password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @param password new value of password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Get the value of userName
+     *
+     * @return the value of userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * Set the value of userName
+     *
+     * @param userName new value of userName
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+}
