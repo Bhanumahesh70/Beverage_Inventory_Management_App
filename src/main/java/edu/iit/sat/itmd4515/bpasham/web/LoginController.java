0ee3b52515ff33850b9197f79096557b9a458480
Bhanumahesh <bhanumahesh70@gmail.com>
2024-04-01 21:08:33 -0500

    lab-8 user authentication added

--- src/main/java/edu/iit/sat/itmd4515/bpasham/web/LoginController.java
+++ src/main/java/edu/iit/sat/itmd4515/bpasham/web/LoginController.java
@@ -4,9 +4,21 @@
  */
 package edu.iit.sat.itmd4515.bpasham.web;
 
+import edu.iit.sat.itmd4515.bpasham.security.User;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import jakarta.security.enterprise.AuthenticationStatus;
+import jakarta.security.enterprise.SecurityContext;
+import jakarta.security.enterprise.authentication.mechanism.http.AuthenticationParameters;
+import jakarta.security.enterprise.credential.Credential;
+import jakarta.security.enterprise.credential.Password;
+import jakarta.security.enterprise.credential.UsernamePasswordCredential;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import static jakarta.ws.rs.core.Response.status;
 import java.util.logging.Logger;
 
 /**
@@ -19,17 +31,75 @@ public class LoginController {
 
     private static final Logger LOG = Logger.getLogger(LoginController.class.getName());
 
+    @Inject SecurityContext securityContext;
+    @Inject FacesContext facesContext;
     public LoginController() {
     }
     
+    //model
+        private User user;
+
+    
+
     @PostConstruct
     private void postConstruct(){
         LOG.info("LoginController.postConstruct");
+        
+        //initialize the model
+        user = new User();
     }
     
-    //action method
+    //action methods
     public String doLogin(){
         LOG.info("LoginController.doLogin");
-        return "/welcome.xhtml";
+        HttpServletRequest request = (HttpServletRequest)facesContext.getExternalContext().getRequest();
+        HttpServletResponse response= (HttpServletResponse)facesContext.getExternalContext().getResponse();
+        Credential cred = new UsernamePasswordCredential(this.user.getUserName(), new Password(this.user.getPassword()));
+        
+       AuthenticationStatus status= securityContext.authenticate(request,response,AuthenticationParameters.withParams().credential(cred));
+        
+       switch(status){
+           case SUCCESS:
+               LOG.info(status.toString());
+               break;
+              
+           case SEND_FAILURE:
+               LOG.info("FAILURE!" + status.toString());
+               return "/error.xhtml";
+            
+           case NOT_DONE:
+               LOG.info("NOT DONE!" + status.toString());
+               return "/error.xhtml";
+               
+           case SEND_CONTINUE:
+               LOG.info(status.toString());
+               break;
+               
+               
+       } 
+       return "/welcome.xhtml?faces-redirect=true";
+    }
+    
+    public String doLogout(){
+        LOG.info("LoginController.doLogout");
+        return "/login.xhtml";
+    }
+    
+    /**
+     * Get the value of user
+     *
+     * @return the value of user
+     */
+    public User getUser() {
+        return user;
+    }
+
+    /**
+     * Set the value of user
+     *
+     * @param user new value of user
+     */
+    public void setUser(User user) {
+        this.user = user;
     }
 }
